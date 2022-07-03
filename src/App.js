f50e7b6fcdf10029f53ab9484f653a06588c34e7
johnrybo <john.rybo@gmail.com>
2022-07-03 23:07:10 +0200

    set up layout and config theme for initial state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  return <div>Simma Lugnt</div>;
+  const [email, setEmail] = useState('');
+
+  const validateEmail = (e) => {
+    e.preventDefault();
+    console.log(email);
+  };
+
+  return (
+    <div className='flex items-end justify-center w-full min-h-screen bg-wave bg-cover bg-center theme-white'>
+      <div className='w-[342px] max-w-full m-6 px-6 py-[72px] rounded-boxRadius bg-primary'>
+        <h1 className='text-lg text-secondary font-bold leading-[48px] tracking-tight'>
+          Sign up to our newsletter
+        </h1>
+        <p className='mt-4 text-sm text-secondary font-bold leading-5'>
+          Lorem ipsum dolor sit amet, consecte adipiscing elit praesent sodales
+          purus magna, eget lacinia sapien hendrerit.
+        </p>
+        <form
+          onSubmit={validateEmail}
+          className='flex justify-between w-full h-12 mt-6 py-1 pl-6 pr-1 border-4 border-solid border-secondary rounded-inputRadius '
+        >
+          <input
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='Email'
+            className='w-full text-md leading-8 tracking-tight placeholder-secondary text-secondary bg-primary'
+          ></input>
+          <button
+            type='submit'
+            className='ml-2 px-4 py-1 bg-secondary text-sm text-primary leading-6 tracking-tight whitespace-nowrap rounded-buttonRadius'
+          >
+            sign up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,40 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontSize: {
+      lg: ['40px'],
+      md: ['24px'],
+      sm: ['16px'],
+    },
+    letterSpacing: {
+      tight: '-0.03em',
+    },
+    extend: {
+      textColor: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+      },
+      backgroundColor: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+      },
+      borderColor: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+      },
+      placeholderColor: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+      },
+      backgroundImage: {
+        wave: "url('./images/wave.gif')",
+      },
+      borderRadius: {
+        boxRadius: '40px',
+        inputRadius: '1e+06px',
+        buttonRadius: '24px',
+      },
+    },
   },
   plugins: [],
 };
